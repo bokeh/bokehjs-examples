ee52f153f4ebc325600bd6d38d5136e9c9ad82d3
Stefan van der Walt <stefanv@berkeley.edu>
2021-09-14 21:35:52 -0700

    Refactor example to use npm package

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -10,6 +10,38 @@ module.exports = {
   },
   resolve: {
     mainFields: ["main", "module"],
+    alias: {
+      bokehjs: path.resolve(
+        __dirname,
+        "node_modules/@bokeh/bokehjs/build/js/lib"
+      )
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        loader: "babel-loader",
+        include: /static\/js/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js?$/,
+        include: /node_modules\/@bokeh/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              plugins: [
+                "@babel/plugin-proposal-export-namespace-from",
+                "@babel/plugin-proposal-optional-chaining",
+                "@babel/plugin-proposal-nullish-coalescing-operator"
+              ],
+            },
+          }
+        ]
+      }
+    ]
   },
   plugins: [new webpack.IgnorePlugin(/moment$/)],
 }
