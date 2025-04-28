633e4cf77f0bf771f53c43c0786fbb0016b6b9b8
Ian Thomas <ianthomas23@gmail.com>
2025-04-28 17:36:13 +0100

    Add Angular example (#35)

---
+++ ci/typescript/create_angular_ng.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+
+set -eux
+
+export OUTPUT_DIRECTORY=../temp/typescript/angular_ng
+
+mkdir -p $OUTPUT_DIRECTORY
+cd $OUTPUT_DIRECTORY
+rm -rf *
+
+function merge-json() {
+  # merge the second json file into the first.
+  TEMP_FILE=$(mktemp)
+  jq '. * input' $1 $2 > TEMP_FILE && mv TEMP_FILE $1
+}
+
+# 1. Install @angular/cli
+npm install -g @angular/cli
+
+# 2. Create Angular application
+ng new angular_ng --directory ./ --minimal --routing=false --skip-git --ssr=false --style=css
+
+# 3. Build and serve the initial project
+# npm run build
+# # npm run start
+# In a web browser navigate to http://localhost:4200/
+
+# 4. Add BokehJS dependency to this project. This assumes the package has been built and copied to the root directory of this repository as outlined in the top-level README.md.
+npm install ../../../../bokeh-bokehjs-3.8.0-dev.1.tgz
+
+# 5. Create a new file src/app/bokeh-js/bokeh-js.component.ts containing a BokehJS plot component
+mkdir -p src/app/bokeh-js
+cat > src/app/bokeh-js/bokeh-js.component.ts << EOF
+import { Component, OnInit } from '@angular/core'
+import * as Bokeh from "@bokeh/bokehjs";
+
+function create_bokehjs_plot(): Bokeh.Column {
+  const source = new Bokeh.ColumnDataSource({data: { x: [0.1, 0.9], y: [0.1, 0.9], size: [40, 10] }});
+
+  const plot = Bokeh.Plotting.figure({
+    title: "Example BokehJS plot", height: 500, width: 500,
+    x_range: [0, 1], y_range: [0, 1], sizing_mode: "stretch_width",
+  });
+
+  plot.scatter({ field: "x" }, { field: "y" }, {source, size: { field: "size" }});
+
+  const button = new Bokeh.Widgets.Button({label: "Click me to add a point", button_type: "primary"});
+  function button_callback() {
+    const data = source.data as any;
+    data.x.push(Math.random());
+    data.y.push(Math.random());
+    data.size.push(10 + Math.random()*30);
+    source.change.emit();
+  }
+  button.on_click(button_callback);
+
+  return new Bokeh.Column({children: [plot, button], sizing_mode: "stretch_width"});
+}
+
+@Component({
+  selector: 'app-bokeh-js',
+  imports: [],
+  template: \`<div id="target"></div>\`,
+  styles: \`\`
+})
+
+export class BokehJSComponent implements OnInit {
+  ngOnInit() {
+    console.info("BokehJS version:", Bokeh.version);
+    Bokeh.Plotting.show(create_bokehjs_plot(), "#target");
+  }
+}
+EOF
+
+# 6. Replace src/app/app.component.ts so that it uses the BokehJSComponent
+cat > src/app/app.component.ts << EOF
+import { Component } from '@angular/core'
+import { BokehJSComponent } from './bokeh-js/bokeh-js.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [BokehJSComponent],
+  template: \`<app-bokeh-js></app-bokeh-js>\`,
+  styles: [],
+})
+
+export class AppComponent {}
+EOF
+
+# 7. Remove some build warnings by allowing non ESM imports by adding to angular.json
+cat > temp.json << EOF
+{
+  "projects": {
+    "angular_ng": {
+      "architect": {
+        "build": {
+          "options": {
+            "allowedCommonJsDependencies": [
+              "@bokeh/bokehjs",
+              "mathjax-full",
+              "regl"
+            ]
+          }
+        }
+      }
+    }
+  }
+}
+EOF
+merge-json angular.json temp.json
+rm temp.json
+
+# 8. Remove bundle size limits in angular.json by setting the "budgets" to an empty array
+cat > temp.json << EOF
+{
+  "projects": {
+    "angular_ng": {
+      "architect": {
+        "build": {
+          "configurations": {
+            "production": {
+              "budgets": [
+              ]
+            }
+          }
+        }
+      }
+    }
+  }
+}
+EOF
+merge-json angular.json temp.json
+rm temp.json
+
+# 9. Rebuild and Serve the project
+npm run build
+# npm run start
+# In a web browser navigate to http://localhost:4200/
